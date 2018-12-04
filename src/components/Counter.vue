b6e6019be74bcee938d1e13c0ca98c0d517b8139
2353769123@qq.com <2353769123@qq.com>
2018-12-05 07:31:58 +0800

    父子组件数据流双向流动

---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <button @click="$emit('increment')">+</button>
+      <button @click="$emit('decrement')">-</button>
+      <p>{{num}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  props:['num']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>{{num}}</p>
+    <Counter :num="num" @increment="increment" @decrement="decrement"></Counter>
+  </div>
+</template>
+
+<script>
+import Counter from './Counter'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '父子组件双向通信',
+      num: 10
+    }
+  },
+  methods:{
+    increment(){
+      this.num++;
+    },
+    decrement(){
+      this.num--;
+    }
+  },
+  components:{
+    Counter
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
