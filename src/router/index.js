73cebbb6fb9e3911af6f43c7237e20f76262c89f
2353769123@qq.com <2353769123@qq.com>
2018-12-07 08:01:23 +0800

    嵌套路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from './../views/GoodsList'
+import Title from '@/views/Title'
+import Image from '@/views/Image'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {  // 动态路由
-      path: '/goods/:goodsId/user/:name',
+      path: '/goods/',
       name: 'GoodsList',
-      component: GoodsList
+      component: GoodsList,
+      children:[    // goods下面的子路由
+          {
+              path: 'title',
+              name: 'title',
+              component: Title
+          },
+          {
+            path: 'img',
+            name: 'img',
+            component: Image
+          }
+      ]
     }
   ]
 })
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -3,6 +3,11 @@
    这是商品列表页面
   <span>{{$route.params.goodsId}}</span><br/>
   <span>{{$route.params.name}}</span>
+  <router-link to="/goods/title">显示商品标题</router-link>
+  <router-link to="/goods/img">显示商品图片</router-link>
+  <div>
+       <router-view></router-view>
+  </div>
   </div>
 </template>
 
