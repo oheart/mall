a9513fcea4cf379ad6338ccb6034e9c030e7215a
2353769123@qq.com <2353769123@qq.com>
2018-12-17 22:27:19 +0800

    登录

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -9,6 +11,36 @@ router.get('/', function(req, res, next) {
 router.get('/test', function(req, res, next) {
   res.send('test');
 });
+
+router.post('/login', function(req, res, next){
+  var param = {
+    userName: req.body.userName,
+    userPwd: req.body.userPwd
+  }
+  User.findOne(param, function(err, doc){
+    if(err){
+      res.json({
+        status: "1",
+        msg: err.message
+      })
+    }else{
+      if(doc){
+        res.cookie("userId", doc.userId,{
+          path: '/',
+          maxAage: 1000*60*60
+        })
+        // req.session.user = doc;
+        res.json({
+          status: "0",
+          msg: "",
+          result: {
+            userName: doc.userName
+          }
+        })
+      }
+    }
+  })
+})
 
 
 module.exports = router;
