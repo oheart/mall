20bcc5eaf124b15b019e38e96fdee543ac673d65
2353769123@qq.com <2353769123@qq.com>
2018-12-19 21:35:09 +0800

    购物车列表

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -47,7 +47,7 @@
                   </li>
                   <li class="regi_form_input noMargin">
                     <i class="icon IconPwd"></i>
-                    <input type="password" tabindex="2"  name="password"  v-model="userPwd" class="regi_login_input regi_login_input_left login-input-no input_text" placeholder="Password" >
+                    <input type="password" tabindex="2"  name="password"  v-model="userPwd" class="regi_login_input regi_login_input_left login-input-no input_text" placeholder="Password" @keyup.enter="login">
                   </li>
                 </ul>
               </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import GoodsList from './../views/GoodsList'
+import GoodsList from '../views/GoodsList'
+import Cart from '../views/Cart'
 
 Vue.use(Router)
 
@@ -9,6 +10,11 @@ export default new Router({
     {
       path: '/',
       component: GoodsList
+    },
+    {
+      path: '/Cart',
+      name: 'Cart',
+      component: Cart
     }
   ]
 })
