f28f0ce15a71515d604f04e5992e6707325a0390
2353769123@qq.com <2353769123@qq.com>
2018-12-23 20:41:05 +0800

    订单成功页功能实现

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import GoodsList from '../views/GoodsList'
 import Cart from '../views/Cart'
 import Address from '../views/Address'
 import OrderConfirm from '../views/OrderConfirm'
+import OrderSuccess from '../views/OrderSuccess'
 
 Vue.use(Router)
 
@@ -27,6 +28,11 @@ export default new Router({
       path: '/orderConfirm',
       name: 'OrderConfirm',
       component: OrderConfirm
+    },
+    {
+      path: '/orderSuccess',
+      name: 'OrderSuccess',
+      component: OrderSuccess
     }
   ]
 })
